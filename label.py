ff0ca49b18c9cbafb33aae29cba7a0b2676ca7cb
duinodu <472365351duino@gmail.com>
2017-07-18 10:17:57 +0800

    save xml

--- label.py
+++ label.py
@@ -13,21 +13,24 @@ import os, sys
 import cv2
 import numpy as np
 import math
+import copy
+import xml.etree.ElementTree as ET
 
 
 x1,y1, x2, y2 = 0,0,0,0
+state = 0
+# ellipse parameter
 x0, y0 = 0, 0
 angle  = 0
 a, b = 0, 0
-state = 0
 
 im = np.zeros((100,100,3))
+filename = ""
 
 def onmouse(event, x, y, flags, param):
     global im
     global x1, y1, x2, y2
-    global x0, y0, angle
-    global a, b
+    global x0, y0, a, b, angle
     global state
 
     img = im.copy()
@@ -86,9 +89,7 @@ def onmouse(event, x, y, flags, param):
 
 def clear():
     global x1, y1, x2, y2
-    global x0, y0
-    global angle
-    global a, b
+    global x0, y0, a, b, angle
     global state
 
     x1,y1, x2, y2 = 0,0,0,0
@@ -98,21 +99,104 @@ def clear():
     state = 0
 
 
+# xml template
+Annnotation = """<annotation>
+	<folder>FetalHead</folder>
+	<filename>{}</filename>
+	<source>
+		<database>The VOC2007 Database</database>
+		<annotation>PASCAL VOC2007</annotation>
+		<image>flickr</image>
+		<flickrid>341012865</flickrid>
+	</source>
+	<size>
+		<width>{}</width>
+		<height>{}</height>
+		<depth>3</depth>
+	</size>
+	<segmented>0</segmented>
+        {}
+</annotation>
+"""
+Object = """
+	<object>
+		<name>{}</name>
+		<pose>Left</pose>
+		<truncated>0</truncated>
+		<difficult>0</difficult>
+		<bndbox>
+                        <x>{}</x>
+                        <y>{}</y>
+                        <a>{}</a>
+                        <b>{}</b>
+                        <angle>{}</angle>
+		</bndbox>
+	</object>
+"""
+
+
+def save():
+    global x0, y0, a, b, angle
+    global filename
+    global im
+    height, width = im.shape[:2] 
+    name = 'head'
+    newObj = copy.deepcopy(Object).format(name, int(x0), int(y0), int(a), int(b), int(angle))
+    newAnno = copy.deepcopy(Annnotation).format(filename, width, height, newObj)
+    xmlfile = os.path.join(annodir, '/{}.xml'.format(filename))
+    if os.path.exists(xmlfile):
+        os.remove(xmlfile)
+    with open(xmlfile, 'w') as fid:
+        fid.write(newAnno)
+    print('Image No. {} saved.'.format(eval(filename)))
+    
+
+def load(xmlfile):
+    global x0, y0, a, b, angle
+    global im
+    img = im.copy()
+
+    tree = ET.parse(xmlfile)
+    width = int(tree.find('size').find('width').text)
+    height = int(tree.find('size').find('height').text)
+    objs = tree.findall('object')
+    for index, obj in enumerate(objs):
+        name = obj.find('name').text.lower()
+        bbox = obj.find('bndbox')
+        x0 = int(bbox.find('x').text)
+        y0 = int(bbox.find('y').text)
+        a = int(bbox.find('a').text)
+        b = int(bbox.find('b').text)
+        angle = int(bbox.find('angle').text)
+        cv2.ellipse(img,(x0,y0),(a, b), angle, 0,360, (0,255,255))
+
+
+
+
 def label(argv):
     root = os.path.abspath(argv[1])
-    imgfiles = sorted([os.path.join(root, x) for x in sorted(os.listdir(root)) if x.endswith('.JPG') or x.endswith('.jpg')])
-    annodir = os.path.join(root, '../Annotations')
+    if not os.path.exists(os.path.join(root, 'JPEGImages')):
+        print("Use https://github.com/DuinoDu/BBox-Label-Tool/blob/master/tools/createDS.py to convert images to voc-format")
+        return
+
+    imgdir = os.path.join(root, 'JPEGImages')
+    imgfiles = sorted([os.path.join(imgdir, x) for x in sorted(os.listdir(imgdir)) if x.endswith('.JPG') or x.endswith('.jpg')])
+
+    annodir = os.path.join(root, 'Annotations')
     if not os.path.exists(annodir):
         os.makedirs(annodir)
     
     global im
+    global filename
     cv2.namedWindow('img')
     cv2.setMouseCallback('img', onmouse)
 
     for f in imgfiles:
         print(f)
+        filename = os.path.basename(f)[:-4]
         im = cv2.imread(f)
-        cv2.imshow('img', im)
+        load(os.path.join(annodir, '{}.xml'.format(filename)))
+        #cv2.imshow('img', im)
 
         while True:
             ch = cv2.waitKey(0) & 0xff
@@ -126,13 +210,10 @@ def label(argv):
                 cv2.imshow('img', im)
                 clear()
             elif ch == ord(' '):
-                #save()
-                pass
+                save()
 
         if ch == ord('q'):
             break
-
-state = 0
 
 def main():
     import sys
